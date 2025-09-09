4a552f09de91508477e0f4bf080ef734ce781ff6
TBDC <sio.bureauducolombier@gmail.com>
2025-09-09 17:32:37 +0200

    absolute path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO>/' // Remplacez <REPO> par le nom de votre repo GitHub
+  base: '/pomodoro-pages/' // Chemin correct pour le repo GitHub Pages
 })
